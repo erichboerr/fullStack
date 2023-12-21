f953aca9053ad170cd35bbe212c5c43fe96f52dd
Erich <erichboerr@hotmail.com>
2023-12-21 02:06:17 -0300

    limpiando proyecto footer

--- frontend/src/assets/components/menu.jsx
+++ frontend/src/assets/components/menu.jsx
@@ -5,26 +5,25 @@ const Menu = () => {
   return (
     <div className="container text-center">
       <div className="row justify-content-around p-3">
-      <ul id="menu">
-      <li>
-        <Link to="/contacto">Contacto</Link>
-      </li>
-      <li>
-        <Link to="/quienes-somos">Quienes somos</Link>
-      </li>
-      <li>
-        <Link to="/catalogo">Productos</Link>
-      </li>
-      <li>
-        <a href="#">Preguntas frecuentes</a>
-      </li>
-      <li>
-        <a href="#">Politica de reembolsos</a>
-      </li>
-    </ul>
+        <ul id="menu">
+          <li>
+            <Link to="/contacto">Contacto</Link>
+          </li>
+          <li>
+            <Link to="/quienes-somos">Quienes Somos</Link>
+          </li>
+          <li>
+            <Link to="/catalogo">Productos</Link>
+          </li>
+          <li>
+            <a href="#">Preguntas frecuentes</a>
+          </li>
+          <li>
+            <a href="#">Politica de reembolsos</a>
+          </li>
+        </ul>
       </div>
-    </div> 
-    
+    </div>
   );
 };
 
